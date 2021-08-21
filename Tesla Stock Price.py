714149561dd434aff4ee47760d38b9b4c258e427
Yagya Dutt Sharma <66865453+yagyaton@users.noreply.github.com>
2021-08-22 00:07:45 +0530

    Update Tesla Stock Price.py

--- Tesla Stock Price.py
+++ Tesla Stock Price.py
@@ -89,14 +89,5 @@ if percentage>=THRESHPRECENTAGE:
     print(message.status)
 #---------------------------------------------------------------------------------------------------------------------------------#
 
-#Optional: Format the SMS message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+
 
